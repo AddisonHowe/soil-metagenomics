2ef5cdddb5cf1629b7145110270f141163bc1ab4
Joelandsittel37 <landsittel@u.northwestern.edu>
2025-06-24 15:33:02 -0500

    6 24 updates

---
+++ pH_perturbations2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --account=p32818
+#SBATCH --partition=normal
+#SBATCH --job-name=pH_perturbation     # Job name
+#SBATCH --ntasks=1                     # Number of tasks 
+#SBATCH --cpus-per-task=4              # Number of CPU cores per task
+#SBATCH --mem=32G                       # Total memory
+#SBATCH --time=08:00:00                # Time limit (hh:mm:ss)
+
+
+DIR='/projects/p32818/metagenomic_data'
+
+cd $DIR
+
+# Run script
+python pH_perturbations2.py 
