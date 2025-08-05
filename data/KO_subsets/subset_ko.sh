476400d4eeaf051b6372ca1b576a2f5602544eac
Addison Howe (from Quest) <addi.howe@gmail.com>
2025-08-05 14:25:53 -0500

    updated ko subsetting

---
+++ data/KO_subsets/subset_ko.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#=============================================================================
+#
+# FILE: subset_ko.sh
+#
+# USAGE: subset_ko.sh koval
+#
+# DESCRIPTION: Wrapper to subset a KO value.
+#
+# EXAMPLE: sh subset_ko.sh <koval>
+#=============================================================================
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 koval"
+    exit 1
+fi
+
+koval=$1
+annotation_dir=data/raw_data/coassembly
+bedfiledir=/scratch/aie7773
+
+echo KO="${koval}"
+outdir=data/KO_subsets/subset_${koval}
+
+echo Subsetting annotation data...
+sh scripts/subset_annotation_data.sh $koval $annotation_dir $outdir
+
+echo Subsetting bed files...
+sh scripts/subset_bed_files.sh $koval $bedfiledir $outdir
+
+echo Done!
