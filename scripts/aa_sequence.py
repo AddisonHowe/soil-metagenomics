29feedbaf54241f0756cbaa9f075e39480b8a1bc
Joelandsittel37 <landsittel@u.northwestern.edu>
2025-08-06 12:51:18 -0500

    script change

--- scripts/aa_sequence.py
+++ scripts/aa_sequence.py
@@ -6,15 +6,21 @@ cluster_IDs = pd.read_csv('../out/orf_ids/cluster_ids_09_K00362.tsv', sep='\t', 
 cluster_IDs = set(cluster_IDs[0].values)  # Using a set for faster lookups
 
 orf_sequence_pairs = []
+found_ids = []
 
 # Process the FASTA file record by record
 with open("../data/raw_data/coassembly/all.coassembly_proteins_1st_ClusterDB_repseq.fasta", "r") as fasta_file:
     for record in SeqIO.parse(fasta_file, "fasta"):
         if record.id in cluster_IDs:
+            found_ids.append(record.id)
             orf_sequence_pairs.append({'orf': record.id, 'aa_sequence': str(record.seq)})
             print(record.id, 'sequence found', record.seq)
 
 # Save to TSV
 print('success, storing at ../out/aaseqs/orf_sequences_09_K00362.tsv')
 df = pd.DataFrame(orf_sequence_pairs)
-df.to_csv('../out/aaseqs/orf_sequences_09_K00362.tsv', sep='\t', index=False)
+df.to_csv('../out/aaseqs/orf_sequences_K00362_90.tsv', sep='\t', index=False)
+
+missing_ids = cluster_IDs - found_ids
+print(f"{len(missing_ids)} IDs in cluster list not found in FASTA:")
+print(list(missing_ids)[:10])
