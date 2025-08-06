728a062fb632084eeafa064806f418fe7855f4c5
Joelandsittel37 <landsittel@u.northwestern.edu>
2025-08-06 11:52:48 -0500

    sequences for nirB

--- scripts/aa_sequence.py
+++ scripts/aa_sequence.py
@@ -8,12 +8,13 @@ cluster_IDs = set(cluster_IDs[0].values)  # Using a set for faster lookups
 orf_sequence_pairs = []
 
 # Process the FASTA file record by record
-with open("../data/raw_data/all.coassembly_proteins_1st_ClusterDB_repseq.fasta", "r") as fasta_file:
+with open("../data/raw_data/coassembly/all.coassembly_proteins_1st_ClusterDB_repseq.fasta", "r") as fasta_file:
     for record in SeqIO.parse(fasta_file, "fasta"):
         if record.id in cluster_IDs:
             orf_sequence_pairs.append({'orf': record.id, 'aa_sequence': str(record.seq)})
             print(record.id, 'sequence found', record.seq)
 
 # Save to TSV
+print('success, storing at ../out/aaseqs/orf_sequences_09_K00362.tsv')
 df = pd.DataFrame(orf_sequence_pairs)
 df.to_csv('../out/aaseqs/orf_sequences_09_K00362.tsv', sep='\t', index=False)
--- scripts/aa_sequence.sh
+++ scripts/aa_sequence.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1                     # Number of tasks 
 #SBATCH --cpus-per-task=4              # Number of CPU cores per task
 #SBATCH --mem=120G                     # Total memory
-#SBATCH --time=47:00:00                # Time limit (hh:mm:ss)
+#SBATCH --time=10:00:00                # Time limit (hh:mm:ss)
 
 
 DIR='/projects/p32818/metagenomic_data/scripts'
